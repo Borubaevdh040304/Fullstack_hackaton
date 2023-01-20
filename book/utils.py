927adf9715adc18024132bd04a1f87556bd622be
Nuraiym0 <nuraiymmamytova0@gmail.com>
2023-01-20 12:27:02 +0600

    google deploy

--- book/utils.py
+++ book/utils.py
@@ -5,3 +5,4 @@ def send_activation_mail(email, activation_code):
     activation_link = f'http://35.185.69.40/account/activate/{activation_code}/'
     message = f'Activate your account with a link:\n{activation_link}'
     send_mail("Activate account", message, 'admin@admin.com', recipient_list=[email], fail_silently=False)
+
